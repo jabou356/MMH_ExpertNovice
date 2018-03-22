a16cb5642e30c7b16f53c5394e24232887be01c3
Unknown <jason.bouffard@mail.mcgill.ca>
2018-03-22 12:45:47 -0400

    Clean data

--- Fatigue/rMMH_EMGanalysis.m
+++ Fatigue/rMMH_EMGanalysis.m
@@ -6,19 +6,41 @@ GenericPath_EN
 %% Get participants
 load(Path.SubjectID);
 
-for isujet = [18 19 21 24 25 26 29 30 31]%length(Alias.pseudo):-1:1
+for isujet = length(Alias.pseudo)%[18 19 21 24 25 26 29 30 31]%length(Alias.pseudo):-1:1
     
     %% For each trial, run EMG compute (filtering, RMS)
     load([Path.RepetitiveMMH, Alias.pseudo{isujet}, '.mat']);
+    disp(['data loaded: Subject # ', num2str(isujet), Alias.pseudo{isujet}]) 
     
     % Process emg (bandpass filter + RMS)
     data=rMMH_emg_compute(data,2000);
-    
+    disp(['data processed: Subject # ', num2str(isujet), Alias.pseudo{isujet}]) 
+
 
     
     %Partition data based on the presence of a box 
     data = PartionRMMH(data, {'deltant', 'biceps'});
-    
+    disp(['data partitionned: Subject # ', num2str(isujet), Alias.pseudo{isujet}]) 
+
+    for imuscle = 1:size(data{1}.NormRMS,2)
+        
+        trialID.trial=[];
+        trialID.box=[];
+        
+        tovalidate = [];
+        for itrial = 1:length(data)
+            trialID.trial = [trialID.trial,repmat(itrial,1, size(data{itrial}.box,1))];
+            trialID.box = [trialID.box, 1:size(data{itrial}.box,1)];
+            
+            
+            tovalidate = [tovalidate, squeeze(data{1,itrial}.NormRMS(:,imuscle,:))];
+            
+        end
+        
+        data = clean_datarMMH(data,imuscle,Alias.pseudo{isujet},tovalidate,trialID);
+
+    end 
+        
     % Process emg time-frequency
  %   data = TimeFreqEMG(data);
 %   for itrial=1:length(data)
